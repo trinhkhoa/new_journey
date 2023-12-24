5a65dbe3697e7f6a663434d10d219600667c2ab9
brIan_trInh <trinh_k1@denison.edu>
2023-12-24 20:46:03 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="App centered">
+      <img src="/falling2.gif" alt="falling2" />
+      <p>
+        Khi nhắc đến trò chơi điện tử, có lẽ đa số chúng ta sẽ nghĩ đến sự tàn bạo. Tuy nhiên, điều này không hoàn toàn đúng. Bài viết này sẽ đưa các bạn đến với một mặt khác của trò chơi điện tử. Đây là thế giới dưới lòng đất mà Toby Fox và Temmie Chang đã tạo ra, nơi mà bạn có quyền lựa chọn. Khi vận mệnh của cả thế giới nằm trong tay bạn, liệu bạn sẽ có đủ kiên nhẫn làm ra chiếc bánh ngọt cho mình, hay bạn vẫn sẽ đi theo lối mòn của sự bạo lực?
+      </p>
+      <img className="pie-copy" src="/pie-copy1.gif" alt="pie-copy" />
+      <img className="n1" src="/n1.png" alt="pie" />
+      <p>
+        Lạc vào thế giới dưới lòng đất, bạn sẽ được tiếp đón bởi Flowey. Nhân vật mở đầu mang đến ấn tượng thân thiện cho người chơi qua cái tên “Flowey” được chơi chữ từ “Flower”, tức là hoa. Không chỉ vậy, tạo hình nhân vật này là một bông hoa với cánh tròn đáng yêu, kết hợp với màu vàng tươi chủ đạo giúp cho người chơi có cảm giác được chào đón với năng lượng tích cực. Năng lượng ấy sớm trở nên nguội lạnh khi Flowey lộ rõ bản chất thật.
+      </p>
+      <div className="content-row">
+        <p className="text-left">
+          Hành trình của người chơi dần được mở ra từ sau cuộc gặp gỡ với Flowey. Khi bị tấn công bởi bông hoa ấy, người chơi được cứu bởi Toriel (được chơi chữ từ “Tutorial”, tức là hướng dẫn). Đây cũng là lúc người chơi được dẫn tới mái ấm mới. Tuy nhiên lúc này mong muốn quay trở về nhà của nhân vật chính bỗng trỗi dậy. Chính vì vậy, dù được Toriel hết mực yêu thương chăm sóc, nhân vật mà người chơi điều kiển vẫn rời khỏi mái ấm ấy và đi trên cuộc phiêu lưu đầy khó khăn và hiểm trở.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="book-container">
+          <img className="book" src="/book.gif" alt="book" />
+        </div>
+      </div>
+      <div className="content-row">
+        <p className="text-right">
+          Tương tự như trong các trò chơi điện tử khác, đặc biệt là thể loại nhập vai, người chơi phải đối mặt với rất nhiều nhân vật phụ (hay còn được gọi là NPC - non-playable character) để rời khỏi thế giới dưới lòng đất. Những nhân vật này sẽ đóng vai trò là bậc thang hỗ trợ người chơi nâng “chỉ số” (hay còn được gọi là nâng level - LV) và trở nên mạnh hơn. Những tên “quái vật” này sẽ tìm cách tiếp cận và tấn công bạn, khiến hành trình của bạn càng trở nên gian nan trắc trở hơn.
+        </p>
+        <img className="undyne" src="/undyne.jpg" alt="undyne" />
+      </div>
+      <img className="n2" src="/n2.png" alt="pie" />
+      <p>
+        Ở thế giới dưới lòng đất, tất cả “quái vật” đều có cảm xúc và câu chuyện của riêng chúng. Và cũng tại nơi này, những gì bạn thực hiện đều có hậu quả. Những nhân vật phụ ở đây sẽ thay đổi cách cư xử tuỳ thuộc vào lựa chọn của bạn. Khi bạn thể hiện lòng vị tha, họ sẽ đáp lại bằng sự nhân từ. Ngược lại, nếu bạn quyết định tàn sát, họ sẽ phản kháng và tự vệ. Người chơi vẫn có thể nâng LV như trong những trò chơi khác, nhưng LV ở đây có nghĩa như thế nào sẽ tuỳ thuộc vào con đường bạn chọn.
+      </p>
+      <img className="hr" src="/hourglass-copy14.gif" alt="pie" />
+      <p>
+        Vậy, bạn với tư cách là người chơi sẽ quyết định chọn nâng Level of Violence (chỉ số bạo lực) hay LoVe (chỉ số tình cảm)? Liệu bạn có đủ tố chất vượt qua mọi khó khăn, hiểm trở và giữ lại bản ngã nhân từ? Hay bạn sẽ mất kiên nhẫn để rồi quay lại lối mòn của sự bạo lực và tàn sát hết tất cả cư dân nơi đây? Và nếu bạn chọn con đường ấy thì ai mới thật sự là “quái vật”?
+      </p>
+      <img className="n3" src="/n3.png" alt="pie" />
+      <div className="puddle-container">
+        <img className="puddle" src="/puddle-copy.gif" alt="puddle" />
+        <p className="overlay-text">Trước người chơi, bảy đứa trẻ khác cũng đã lạc vào thế giới dưới lòng đất. Sáu trong số chúng đều gặp chung số phận bị giam giữ linh hồn. Những đứa trẻ sa ngã ấy vì những lựa chọn sai lầm nên đã đi lệch hướng và trả cái giá đắt. Khác với sáu đứa trẻ kia, người chơi là kẻ nắm giữ tương lai của thế giới dưới lòng đất. Truyền thuyết kể rằng khi nhà vua có được đủ số lượng linh hồn, thế giới dưới lòng đất sẽ được giải phóng.</p>
+        <p className="overlay-text2"> Trùng hợp thay, người chơi chính là linh hồn cuối mà nhà vua cần để giúp người dân nơi đây có thể trở nên tự do. Thế nhưng tại sao họ lại mắc kẹt dưới lòng đất như vậy?</p>
+        <p className="overlay-text3"> Đứa trẻ đầu tiên rơi xuống nơi đây đã được nhà vua và hoàng hậu hết mực yêu thương, chăm sóc. Tuy nhiên, đứa trẻ ấy sớm mất do bạo bệnh. Hoàng tử nhỏ tuy đau lòng những vẫn giúp thực hiện ước nguyện viếng thăm quê hương lần cuối của bạn mình.  Trớ trêu thay, dân làng lầm tưởng đứa trẻ ấy mất do quái vật tấn công. Chính vì vậy, họ đã phong ấn quái vật nơi dưới lòng đất.</p>
+      </div>
+      <img className="fl" src="/frisk-and-flowey.gif" alt="pie" />
+      <div className="puddle-container">
+        <p className="overlay-text4">
+          Hỡi kẻ nắm giữ tương lai, bạn sẽ lựa chọn như thế nào trên cuộc hành trình mới nơi thế giới dưới lòng đất? Liệu bạn sẽ có đủ kiên nhẫn làm ra chiếc bánh ngọt cho mình, hay bạn vẫn sẽ đi theo lối mòn của sự bạo lực?
+        </p>
+      </div>
+      <footer>
+        Thực hiện và lên ý tưởng bởi: Lê Hoàng Thảo Trinh <br /> Lập trình: Khoa Trịnh
+      </footer>
     </div>
   );
 }
